5594f262cca98a370e411e2685dc5e4db86fb326
John Michael Zuñiga <mizu.github.em@gmail.com>
2026-06-13 23:58:22 +0800

    Updated vite version

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import react from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   base: process.env["GITHUB_ACTIONS"] != null ? "/portfolio/" : undefined,
-  plugins: [react()],
+  plugins: [
+    react(),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
+  ],
 });
